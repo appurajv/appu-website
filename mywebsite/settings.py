fb8e997447fcddc96d06b79a9c1af85061d6c637
appurajv <appuvijay10@gmail.com>
2021-01-20 09:38:01 +0900

    added whitenoise storage settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mywebsite/static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 """
 try :
     from .local_settings import *
